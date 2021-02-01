0f830e85ea1b246a4229b4f97042a48a3de10171
Riyan30019 <rhL201830019@gmail.com>
2021-02-02 03:18:04 +0600

    adding double vs triple equal js

---
+++ undefined.js
@@ -0,0 +1,22 @@
+let name;
+console.log(name);
+
+function add(x,y){
+    console.log(x+y);
+}
+const sum=add(x,y);
+
+
+function add(x,y){
+    console.log(x+y);
+        return
+}
+const sum=add(x,y);
+
+function add(x,y){
+    console.log(y);
+}
+console.log(12);
+
+const student={name:'hello' ,id:222}
+console.log(student.subject);
